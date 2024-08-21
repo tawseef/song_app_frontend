1700ec0cb385ebe45fac60f7a6cb8dab1c6f6b77
tawseef <tawseefalam1@gmail.com>
2024-08-21 20:17:44 +0530

    api fixed

--- src/api.js
+++ src/api.js
@@ -1,24 +1,24 @@
-// export const track_API = "https://v1.nocodeapi.com/newusererw/spotify/DVSHqzCESzwJvbtX/search?q=track&type=track";
-// export const getAllPlaylist_API = "http://localhost:8082/v1/getAllPlaylists";
-// export const postCreatePlaylist_API_URL = "http://localhost:8082/v1/createPlaylist";
-// export const addTracksToPlaylist_API_URL = "http://localhost:8082/v1/addTracksToPlaylist";
-// export const getAllDataOfAllPlaylist_API_URL = "http://localhost:8082/v1/getTracksDetailsOfAllPlaylists";
-// export const login_API_URL = "http://localhost:8082/v1/login";
-// export const signup_API_URL = "http://localhost:8082/v1/signup";
-// export const deteleTrack_API_URL = "http://localhost:8082/v1/deleteTracksFromPlaylist";
-// export const deteleWholePlaylist_API_URL = "http://localhost:8082/v1/deleteWholePlaylist";
+export const track_API = "https://v1.nocodeapi.com/newuser1121/spotify/DBvOxdITJGnyqMoS/search?q=track&type=track";
+export const getAllPlaylist_API = "http://localhost:8082/v1/getAllPlaylists";
+export const postCreatePlaylist_API_URL = "http://localhost:8082/v1/createPlaylist";
+export const addTracksToPlaylist_API_URL = "http://localhost:8082/v1/addTracksToPlaylist";
+export const getAllDataOfAllPlaylist_API_URL = "http://localhost:8082/v1/getTracksDetailsOfAllPlaylists";
+export const login_API_URL = "http://localhost:8082/v1/login";
+export const signup_API_URL = "http://localhost:8082/v1/signup";
+export const deteleTrack_API_URL = "http://localhost:8082/v1/deleteTracksFromPlaylist";
+export const deteleWholePlaylist_API_URL = "http://localhost:8082/v1/deleteWholePlaylist";
 
 
 
 
-export const track_API = "https://v1.nocodeapi.com/alamtawse/spotify/YvauwBEkEEAEWOFD/search?q=track&type=track";
-export const getAllPlaylist_API = "https://songapp-k8yt.onrender.com/v1/getAllPlaylists";
-export const postCreatePlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/createPlaylist";
-export const addTracksToPlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/addTracksToPlaylist";
-export const getAllDataOfAllPlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/getTracksDetailsOfAllPlaylists";
-export const login_API_URL = "https://songapp-k8yt.onrender.com/v1/login";
-export const signup_API_URL = "https://songapp-k8yt.onrender.com/v1/signup";
-export const deteleTrack_API_URL = "https://songapp-k8yt.onrender.com/v1/deleteTracksFromPlaylist";
-export const deteleWholePlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/deleteWholePlaylist";
+// export const track_API = "https://v1.nocodeapi.com/alamtawse/spotify/YvauwBEkEEAEWOFD/search?q=track&type=track";
+// export const getAllPlaylist_API = "https://songapp-k8yt.onrender.com/v1/getAllPlaylists";
+// export const postCreatePlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/createPlaylist";
+// export const addTracksToPlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/addTracksToPlaylist";
+// export const getAllDataOfAllPlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/getTracksDetailsOfAllPlaylists";
+// export const login_API_URL = "https://songapp-k8yt.onrender.com/v1/login";
+// export const signup_API_URL = "https://songapp-k8yt.onrender.com/v1/signup";
+// export const deteleTrack_API_URL = "https://songapp-k8yt.onrender.com/v1/deleteTracksFromPlaylist";
+// export const deteleWholePlaylist_API_URL = "https://songapp-k8yt.onrender.com/v1/deleteWholePlaylist";
 
 
